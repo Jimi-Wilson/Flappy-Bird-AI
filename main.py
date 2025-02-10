733c6fb1b7a202751258f078d5ca05dec9f72885
Jimi <69767677+Jimi-Wilson@users.noreply.github.com>
2025-02-10 14:04:23 +0000

    Moving floor added

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((336, 700))
 clock = pygame.time.Clock()
 running = True
 
+
+floor = pygame.image.load('assets/base.png')
+x1 = 0
+x2 = 336
 while running:
+    screen.fill("black")
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.blit(floor, (x1, 600))
+    screen.blit(floor, (x2, 600))
 
-    # RENDER YOUR GAME HERE
+    if x1 == -336:
+        x1 += 672
 
-    # flip() the display to put your work on screen
+    if x2 == -336:
+        x2 += 672
+
+    x1 -= 1
+    x2 -= 1
+
     pygame.display.flip()
-
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
